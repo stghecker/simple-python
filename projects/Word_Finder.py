cc23fe90e98b90a667f3623b74a07b231419151a
stghecker <stogiasteo@gmail.com>
2026-04-09 22:40:56 +0300

    Add files via upload

---
+++ projects/Word_Finder.py
@@ -0,0 +1,11 @@
+while True:
+    words = input("Paste or write your text here: ")
+    char = input("Write the character you want to find here: ")
+    count = words.count(char)
+    print("==============================================")
+    print(f"The character '{char}' appears {count} times.")
+    print("==============================================")
+    print("  ")
+    print("  ")
+
+#STGHECKER#
