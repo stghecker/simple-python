ec63aaef34a9e57c963655c897cc0bc9ca617124
stghecker <260478049+stghecker@users.noreply.github.com>
2026-04-11 15:07:03 +0300

    Add countdown timer functionality
    Implement a simple countdown timer that increments every second.

---
+++ projects/Countdown_Timer/Countdown_Timer.py
@@ -0,0 +1,12 @@
+import time
+vctime = 0
+input("Press Enter to start...")
+try:
+    while True:
+        vctime += 1
+        print(vctime)
+        time.sleep(1)
+except:
+    print("ERROR")
+
+#STGHECKER#
