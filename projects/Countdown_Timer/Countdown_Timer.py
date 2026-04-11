f9e9a7952dd8c5b9caa2a79e445a833c0184c650
stghecker <260478049+stghecker@users.noreply.github.com>
2026-04-11 15:12:29 +0300

    Implement countdown timer with user-defined duration
    Refactor countdown timer to accept user input for seconds and display countdown.

--- projects/Countdown_Timer/Countdown_Timer.py
+++ projects/Countdown_Timer/Countdown_Timer.py
@@ -1,12 +1,18 @@
 import time
-vctime = 0
-input("Press Enter to start...")
-try:
-    while True:
-        vctime += 1
-        print(vctime)
-        time.sleep(1)
-except:
-    print("ERROR")
+check = 0
+seconds = input("Give seconds: ")
+
+while check == 0:
+    try:
+        seconds = int(seconds)
+        check = 1
+    except ValueError:
+        seconds = input("Please input a number.")
+
+for i in range(seconds, 0, -1):
+    print(f"{i}...")
+    time.sleep(1)
+
+print("Time's up!")
 
 #STGHECKER#
