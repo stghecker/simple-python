07c26fc7ea1e0a2950c007ee6eba7d0dc03b57ff
stghecker <260478049+stghecker@users.noreply.github.com>
2026-07-02 17:37:58 +0300

    Add HEX color picker functionality
    Implement HEX color input and turtle drawing.

---
+++ projects/HEX_COLOR_PICKER/HEX_COLOR_PICKER.py
@@ -0,0 +1,24 @@
+from turtle import *
+while True:
+    vhex = input("Enter your HEX code here: ").strip()
+    try:
+        len(vhex) < 1
+        if len(vhex) in [3, 6, 8] and all(c in "0123456789abcdefABCDEF" for c in vhex):
+            break
+        else:
+            print("Invalid HEX code")
+    except VallueError:
+        vhex = input("Enter your HEX code here: ").strip()
+
+
+
+vcolor = "#" + vhex
+
+t = Turtle()          
+t.width(5)
+t.color(vcolor)       
+t.shape("circle")
+t.width(1000)
+t.forward(1)
+
+#STGHECKER#
