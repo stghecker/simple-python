0e9ce65f8c2ba335148b736a7399ffbcf89bac52
stghecker <260478049+stghecker@users.noreply.github.com>
2026-07-02 17:51:38 +0300

    Strip leading '#' from HEX code input
    Remove leading '#' from HEX code input for validation.

--- projects/HEX_COLOR_PICKER/HEX_COLOR_PICKER.py
+++ projects/HEX_COLOR_PICKER/HEX_COLOR_PICKER.py
@@ -1,6 +1,6 @@
 from turtle import *
 while True:
-    vhex = input("Enter your HEX code here: ").strip()
+    vhex = input("Enter your HEX code here: ").strip().lstrip("#")
     try:
         len(vhex) < 1
         if len(vhex) in [3, 6, 8] and all(c in "0123456789abcdefABCDEF" for c in vhex):
@@ -8,7 +8,7 @@ while True:
         else:
             print("Invalid HEX code")
     except VallueError:
-        vhex = input("Enter your HEX code here: ").strip()
+        vhex = input("Enter your HEX code here: ").strip().lstrip("#")
 
 
 
