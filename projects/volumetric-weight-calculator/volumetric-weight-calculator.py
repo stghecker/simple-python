4ae62ebf5ec378936a9061a37c7862fe507f4821
stghecker <260478049+stghecker@users.noreply.github.com>
2026-08-10 08:10:16 +0300

    Create volumetric-weight-calculator/volumetric-weight-calculator.py

---
+++ projects/volumetric-weight-calculator/volumetric-weight-calculator.py
@@ -0,0 +1,14 @@
+width = 0
+height = 0
+length = 0
+whl = 0
+
+width = int(input("Width..."))
+height = int(input("Height..."))
+length = int(input("Length..."))
+
+whl = width * height * length 
+
+vfinal = whl / 5
+
+print(vfinal)
