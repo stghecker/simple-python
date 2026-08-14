a2bfaf60d575f4aa337cb68f706c38d5fa9f7164
stghecker <260478049+stghecker@users.noreply.github.com>
2026-08-14 10:51:47 +0300

    Update volumetric-weight-calculator.py

--- projects/volumetric-weight-calculator/volumetric-weight-calculator.py
+++ projects/volumetric-weight-calculator/volumetric-weight-calculator.py
@@ -3,18 +3,46 @@ height = 0.0
 length = 0.0
 whl = 0.0
 grams = 0.0
+while True:
+    while True:
+        width = input("Width...:").replace(",", ".")
+        try:
+            width = float(width)
+            break
+        except ValueError:
+            print("Please input a valid number.")
+    while True:
+        height = input("Height...:").replace(",", ".")
+        try:
+            height = float(height)
+            break
+        except ValueError:
+            print("Please input a valid number.")
+    while True:
+        length = input("Length...:").replace(",", ".")
+        try:
+            length = float(length)
+            break
+        except ValueError:
+            print("Please input a valid number.")
+    while True:
+        grams = input("Grams...:").replace(",", ".")
+        try:
+            grams = float(grams)
+            break
+        except ValueError:
+            print("Please input a valid number.")
 
-width = float(input("Width...:").replace(",", "."))
-height = float(input("Height...:").replace(",", "."))
-length = float(input("Length...:").replace(",", "."))
-grams = float(input("Grams...:").replace(",", "."))
-
-whl = (width * height * length) / 5
+    
 
 
-if whl >= grams:
-    print(f"Your product is {whl:.2f} grams (Volumetric).")
-    print(f"({width} * {height} * {length}) / 5")
-else:
-    print(f"Your product is {grams:.2f} grams (Actual).")   
+    whl = (width * height * length) / 5
+
+
+    if whl >= grams:
+        print(f"Your product is {whl:.2f} grams (Volumetric).")
+        print(f"({width} * {height} * {length}) / 5")
+    elif grams >= whl:
+        print(f"Your product is {grams:.2f} grams (Actual).\n")   
+
 #STGHECKER#
